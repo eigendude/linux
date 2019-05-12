0aa734bcaf24eb65c56a68a7571659b2bb47fc9b
Scott Ellis <scott@jumpnowtek.com>
2019-05-12 05:06:05 -0400

    spidev: Add a generic compatible id

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -668,6 +668,7 @@ static struct class *spidev_class;
 
 #ifdef CONFIG_OF
 static const struct of_device_id spidev_dt_ids[] = {
+	{ .compatible = "userspi" },
 	{ .compatible = "rohm,dh2228fv" },
 	{ .compatible = "lineartechnology,ltc2488" },
 	{ .compatible = "ge,achc" },
